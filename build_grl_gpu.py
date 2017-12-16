facf3414cd41a0c5ddfd6b56c2e9699f8acdd8ae
i.koryakovskiy <i.koryakovskiy@gmail.com>
2017-12-16 10:25:29 +0100

    Renaming docker image

--- build_grl_gpu.py
+++ build_grl_gpu.py
@@ -1,3 +1,3 @@
 import subprocess
-p = subprocess.Popen(["python", "/grl/qt-build/cfg/build.py"], cwd="/grl/qt-build")
+p = subprocess.Popen(["python3", "/grl/qt-build/cfg/build.py"], cwd="/grl/qt-build")
 p.wait()
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
-cp /grl/qt-build/py_env.* /openai
+file="/grl/qt-build/py_env.*"
+if [ -f $file ]; then
+  cp $file /openai
+else
+  echo "$file not found. Please, rebuild GRL."
+fi
+
 exec "$@"
